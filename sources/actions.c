614a1e22a765fc6bb3ac71dfe8bbdf25ed9a4983
Gorkem Sever <gsever@k2m14s06.42kocaeli.com.tr>
2022-08-16 15:52:22 +0300

    first commit

---
+++ sources/actions.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 05:32:09 by gsever            #+#    #+#             */
+/*   Updated: 2022/08/16 13:57:33 by gsever           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file actions.c
+ * @author Gorkem SEVER (gsever)
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "philosophers.h"
+
+/**
+ * @brief Unlocking mutex's job. We can use again this mutex.
+ * 
+ * @return void
+ * @param base 
+ * @fn pthread_mutex_unlock()	: Freeing mutex's job.
+ * @bug Clear.
+ */
+void	leave_forks(t_base *base)
+{
+	pthread_mutex_unlock(&base->fork[base->philos->fork_l]);
+	pthread_mutex_unlock(&base->fork[base->philos->fork_r]);
+}
+
+/**
+ * @brief A philosopher taking forks.
+ * 
+ * @return void
+ * @param base 
+ * @fn pthread_mutex_lock()	: Locking mutex for this mutex on the job.
+ * @fn get_current_time()	: Getting current time.
+ * @fn printf()				: Writing shell -> fork taken.
+ * @bug Clear.
+ */
+void	take_forks(t_base *base)
+{
+	uint64_t	real_time;
+
+	pthread_mutex_lock(&base->fork[base->philos->fork_l]);
+	real_time = get_current_time() - base->start_time;
+	printf("%llums %d %s\n", real_time, base->philos->id, "has taken a left fork");
+	pthread_mutex_lock(&base->fork[base->philos->fork_r]);
+	real_time = get_current_time() - base->start_time;
+	printf("%llums %d %s\n", real_time, base->philos->id, "has taken a right fork");
+}
+
+/**
+ * @brief Philosopher's "SLEEP" function.
+ * 
+ * Creating unsigned long long for real time, we are printing real time,
+ * philosopher's id, and it's sleeping.
+ * 
+ * @param base
+ * @fn get_current_time()	: Getting current time.
+ * @fn printf()				: Writing shell philosopher sleeping.
+ * @bug Clear.
+ */
+void	philo_sleep(t_base *base)
+{
+	uint64_t	real_time;
+
+	real_time = get_current_time() - base->philos->last_eat_time;
+	printf(BLUE"%llums %d %s\n"X, real_time, base->philos->id, "is sleeping");
+	usleep(base->time_to_sleep * 1000);
+}
+
+/**
+ * @brief Philosopher's "EAT" function.
+ * 
+ * Creating unsigned long long for real time, we are printing real time,
+ * philosopher's id, and it's eating.
+ * 
+ * @param base
+ * @fn get_current_time()
+ * @fn printf()
+ * @bug Clear.
+ */
+void	philo_eat(t_base *base)
+{
+	uint64_t	real_time;
+
+	real_time = get_current_time() - base->philos->last_eat_time;
+	printf(GREEN"%llums %d %s\n"X, real_time, base->philos->id, "is eating");
+	base->philos->last_eat_time = real_time;
+	base->philos->eat_count++;
+	usleep(base->time_to_eat * 1000);
+}
+
+/**
+ * @brief Philosopher's "THINKING" function.
+ * 
+ * Creating unsigned long long for real time, we are printing real time,
+ * philosopher's id, and it's thinking.
+ * 
+ * @param base
+ * @fn get_current_time()
+ * @fn printf()
+ * @bug Clear.
+ */
+void	philo_think(t_base *base)
+{
+	uint64_t	real_time;
+
+	real_time = get_current_time() - base->philos->last_eat_time;
+	printf(YELLOW"%lldms %d %s\n"X, real_time, base->philos->id, "is thinking");
+}
---
+++ sources/lifecycle.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lifecycle.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 15:35:12 by gsever            #+#    #+#             */
+/*   Updated: 2022/08/16 15:03:21 by gsever           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	*lifecycle_checker(void *arg)
+{
+	t_base		*base;
+	uint64_t	timestamp;
+	int			i;
+
+	base = (t_base *)arg;
+	i = 0;
+	while (1)
+	{
+		if (base->philos->full_count == base->philos_count)
+			break ;
+		if (i == base->philos_count)
+			i = 0;
+		usleep(100);
+		timestamp = get_current_time() - base->start_time;
+		if (!base->philos[i].full
+			&& ((int)(timestamp - base->philos[i].last_eat_time) > base->time_to_die))
+		{
+			// pthread_mutex_lock(base->)
+			printf("%llu %d %s\n", timestamp, base->philos[i].id, "died");
+			base->is_running = false;
+			break ;
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * @brief 
+ * 
+ * @param arg 
+ * @fn get_current_time()	:
+ * @fn philo_think()		:
+ * @fn usleep()				:
+ * @fn take_forks()			:
+ * @fn philo_eat()			:
+ * @fn philo_sleep()		:
+ * @return void* 
+ * @bug Clear.
+ */
+void	*lifecycle(void *arg)
+{
+	t_base	*base;
+	
+	base = (t_base *)arg;
+	base->philos->last_eat_time = get_current_time();
+	if (base->philos->id % 2 == 1)
+	{
+		philo_think(base);
+		usleep(base->time_to_eat * 0.25 * 1000);
+	}
+	// printf("annenaaaaa\n");
+	while (!base->philos->full)
+	{
+		take_forks(base);
+		philo_eat(base); /* base->philos->eat_count++ */
+		leave_forks(base);
+		philo_think(base);
+		if (base->philos->eat_count == base->must_eat)/* 7 800 200 200 [5] buradaki 5 kac kere donecegini belirtiyor. */
+		{
+			base->philos->full = true;
+			base->philos->full_count++;
+			break ;
+		}
+		philo_sleep(base);
+	}
+	return (NULL);
+}
